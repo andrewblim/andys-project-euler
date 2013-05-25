39ead586a6e1407124eb56dba4a299570a1c9bfa
Andrew Lim <andrew.b.lim@gmail.com>
2013-05-25 12:49:23 -0400

    Finished Python problem 4

--- python3.py
+++ python3.py
@@ -1,5 +1,5 @@
 
-def largestPrime(n):
+def largestPrimeFactor(n):
     i = 2
     while i <= n**0.5:
         if n % i == 0:
@@ -8,4 +8,4 @@ def largestPrime(n):
     return n
 
 if __name__ == '__main__':
-    print(largestPrime(600851475143))
+    print(largestPrimeFactor(600851475143))
