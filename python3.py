1fccdcd30ba42946a70a4cdbb8e305864c3376ad
Andrew Lim <andrew.b.lim@gmail.com>
2013-05-25 11:58:00 -0400

    Added Python solution 3

---
+++ python3.py
@@ -0,0 +1,11 @@
+
+def largestPrime(n):
+    i = 2
+    while i <= n**0.5:
+        if n % i == 0:
+            n = n/i
+        i += 1
+    return n
+
+if __name__ == '__main__':
+    print(largestPrime(600851475143))
